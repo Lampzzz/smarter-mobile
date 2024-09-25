6e1d10c1493582e4c17507f0590b2f500ffc92f1
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-09-25 20:49:08 +0800

    delete: remove all files

--- constants/images.js
+++
@@ -1,4 +0,0 @@
-import whiteLogo from "@/assets/images/white_logo.png";
-import blueLogo from "@/assets/images/blue_logo.png";
-
-export default { whiteLogo, blueLogo };
--- constants/index.js
+++
@@ -1,5 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import fonts from "./fonts";
-
-export { icons, images, fonts };
